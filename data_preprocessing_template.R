d290047ba4af588aabcdf8988fdbedb23ca8bddb
Dimas Putra <112814587+Dimasputra2530@users.noreply.github.com>
2025-01-29 00:52:31 +0700

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -8,8 +8,8 @@ dataset = read.csv('Data.csv')
 library(caTools)
 set.seed(123) #ini untuk mengacak sebuah data
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)#ini membagi data menjadi 80 dan 20
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+training_set = subset(dataset, split == TRUE) #baris yang diberi nilai TRUE dalam split dimasukkan ke training set.
+test_set = subset(dataset, split == FALSE) #Baris yang diberi nilai FALSE dalam split dimasukkan ke test set.
 
 # Feature Scaling
 # training_set = scale(training_set)
